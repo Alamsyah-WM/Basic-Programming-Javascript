a87986970dea95686dda8cfb0da850f1aa027350
Alamsyah Wijaya Massie <111730390+Alamsyah-WM@users.noreply.github.com>
2024-11-01 18:49:19 +0700

    Delete Bab2 directory

--- Bab2/test.js
+++
@@ -1,9 +0,0 @@
-function hello(name, origin = "Bandung") {
-    return `Halo! Nama saya ${name}. Saya tinggal di ${origin}.`;
-}
-
-const rahmat = hello('Rahmat', 'Semarang');
-
-const indra = hello('Indra');
-
-console.log(`${rahmat}\n${indra}`);
